2cdf622f3d85c338e23be6e0692b949a3e82c105
krish parmar <78633764+Krish2643@users.noreply.github.com>
2024-01-17 22:13:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+let card = document.getElementById("card");
+
+function open_popup(){
+      console.log("hello");
+        card.classList.add("open-popup");
+
+}
+
+function close_popup(){
+      card.classList.remove("open-popup");
+}
